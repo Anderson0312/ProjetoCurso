0b04530babe5ba0239fe1d5593512fec1ecafa43
Anderson0312 <74934630+Anderson0312@users.noreply.github.com>
2022-02-19 23:34:10 -0300

    att

---
+++ pagstart/script.js
@@ -0,0 +1,16 @@
+//menu section
+
+let show = true;
+
+const menuSection = document.querySelector ('.container')
+const menubtn = menuSection.querySelector ('.btn-menu')
+
+
+menubtn.addEventListener('click', () => {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+    
+    menuSection.classList.toggle('on', show)
+    show = !show;
+
+})
