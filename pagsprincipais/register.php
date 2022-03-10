0ebd7592474e69e9a1dc1121295bffed62d40a8d
Anderson0312 <andersonmoura812@gmail.com>
2022-03-10 20:21:19 -0300

    Update register.php

--- pagsprincipais/register.php
+++ pagsprincipais/register.php
@@ -28,7 +28,7 @@ if (isset($_POST['send'])) {
     $senha1 = sanitize('senha1', 'email');
     $senha2 = sanitize('senha2', 'email');
     $telefone = sanitize('phone', 'string');
-    $nomedest = sanitize('receber ', 'string');
+    $nomedest = sanitize('receber', 'string');
     $cep = sanitize('cep', 'string');
 
     if ($email === '' or $cpf === '' or $nomecomplet === '' or $datanasci === '' or $genero === '' or $senha1 === '' or $senha2 === '' or $telefone === '' or $nomedest === '' or $cep === '') :
@@ -112,7 +112,7 @@ $page_title = '';
 
 
 // Inclui o cbeçalho da página
-require_once $_SERVER['DOCUMENT_ROOT'] . "/_header.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/phpconfgs/_header.php";
 
 /*********************************************
  * Seu código PHP desta página termina aqui! *
